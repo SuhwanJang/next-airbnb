e6186e1b6857d3007c3f68a08740f72f6b8cc75c
suhwanJang <suhwan9185@gmail.com>
2022-07-03 21:21:09 +0900

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-console': ['off'],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+function Home(): React.ReactNode {
+  return <div />;
+}
+
+export default Home;
