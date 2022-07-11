8c7797f6a38f912822d0bccd64123632d4120146
suhwanJang <suhwan9185@gmail.com>
2022-07-11 23:19:10 +0900

    공통 헤더 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,29 @@
-import {
-  Button,
-  ButtonGroup,
-  Flex,
-  Link,
-  Spacer,
-  Text,
-} from '@chakra-ui/react';
+import { Button, ButtonGroup, Flex, Link, Spacer, Text, useDisclosure } from '@chakra-ui/react';
 import React from 'react';
 import { AiOutlineAliwangwang } from 'react-icons/ai';
+import SignUpModal from './auth/SignUpModal';
 
 function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Flex
-      w="100%"
-      align="center"
-      position="absolute"
-      boxShadow="base"
-      p="6"
-      rounded="md"
-      bg="white"
-    >
-      <Link href="/">
-        <Flex ml="10px" gap="3" fontSize="2xl">
-          <AiOutlineAliwangwang />
-          <Text>airbnb</Text>
-        </Flex>
-      </Link>
-      <Spacer />
-      <ButtonGroup gap="3" mr="10px">
-        <Button type="button" borderRadius="22px">
-          회원가입
-        </Button>
-        <Button type="button" borderRadius="22px">
-          로그인
-        </Button>
-      </ButtonGroup>
-    </Flex>
+    <>
+      <Flex w="100%" align="center" boxShadow="base" p="6" rounded="md" bg="white">
+        <Link href="/">
+          <Flex ml="10px" gap="3" fontSize="2xl">
+            <AiOutlineAliwangwang />
+            <Text>airbnb</Text>
+          </Flex>
+        </Link>
+        <Spacer />
+        <ButtonGroup gap="3" mr="10px">
+          <Button borderRadius="22px" onClick={onOpen}>
+            회원가입
+          </Button>
+          <Button borderRadius="22px">로그인</Button>
+        </ButtonGroup>
+      </Flex>
+      <SignUpModal isOpen={isOpen} onClose={onClose} />
+    </>
   );
 }
 
---
+++ components/auth/SignUpModal.tsx
@@ -0,0 +1,173 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  ModalCloseButton,
+  ModalHeader,
+  InputRightElement,
+  InputGroup,
+  Stack,
+  Select,
+  Flex,
+} from '@chakra-ui/react';
+import { Field, Form, Formik, FormikProps, FieldInputProps } from 'formik';
+import React from 'react';
+
+interface Iprops {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+interface FormValues {
+  email: string;
+  name: string;
+  password: string;
+  month: string;
+  day: string;
+  year: string;
+}
+
+const monthList = Array.from(Array(12), (_i, i) => String(`${i + 1}월`));
+const dayList = Array.from(Array(31), (_, i) => String(`${i + 1}일`));
+const yearList = Array.from(Array(121), (_, i) => String(`${2020 - i}년`));
+
+export default function SignUpModal({ isOpen, onClose }: Iprops) {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader />
+        <ModalCloseButton />
+        <ModalBody>
+          <Formik
+            initialValues={{ email: '', name: '', password: '', month: '', day: '', year: '' }}
+            onSubmit={(values, actions) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                actions.setSubmitting(false);
+              }, 1000);
+            }}
+          >
+            {(props) => (
+              <Form>
+                <Stack spacing={4} marginTop="30px">
+                  <Field name="email">
+                    {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<FormValues> }) => (
+                      <FormControl isInvalid={Boolean(form.errors.email && form.touched.email)}>
+                        <Input {...field} type="email" id="email" placeholder="이메일 주소" />
+                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="name">
+                    {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<FormValues> }) => (
+                      <FormControl isInvalid={Boolean(form.errors.name && form.touched.name)}>
+                        <Input {...field} id="name" placeholder="이름(예: 길동)" />
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="password">
+                    {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<FormValues> }) => (
+                      <FormControl isInvalid={Boolean(form.errors.password && form.touched.password)}>
+                        <InputGroup>
+                          <Input
+                            {...field}
+                            pr="4.5rem"
+                            id="password"
+                            type={show ? 'text' : 'password'}
+                            placeholder="비밀번호 설정하기"
+                          />
+                          <InputRightElement width="4.5rem">
+                            <Button h="1.75rem" size="sm" onClick={handleClick}>
+                              {show ? 'Hide' : 'Show'}
+                            </Button>
+                          </InputRightElement>
+                          <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                        </InputGroup>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Flex gap="3">
+                    <Field name="month">
+                      {({
+                        field,
+                        form,
+                      }: {
+                        field: FieldInputProps<string>;
+                        form: FormikProps<FormValues>;
+                      }) => (
+                        <FormControl id="month">
+                          <Select id="month" onChange={field.onChange}>
+                            {monthList.map((v) => (
+                              <option id="month" value={v}>
+                                {v}
+                              </option>
+                            ))}
+                          </Select>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="day">
+                      {({
+                        field,
+                        form,
+                      }: {
+                        field: FieldInputProps<string>;
+                        form: FormikProps<FormValues>;
+                      }) => (
+                        <FormControl id="day">
+                          <Select id="day" onChange={field.onChange}>
+                            {dayList.map((v) => (
+                              <option id="day" value={v}>
+                                {v}
+                              </option>
+                            ))}
+                          </Select>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="year">
+                      {({
+                        field,
+                        form,
+                      }: {
+                        field: FieldInputProps<string>;
+                        form: FormikProps<FormValues>;
+                      }) => (
+                        <FormControl id="year">
+                          <Select id="year" onChange={field.onChange}>
+                            {yearList.map((v) => (
+                              <option id="year" value={v}>
+                                {v}
+                              </option>
+                            ))}
+                          </Select>
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Flex>
+                </Stack>
+                <Button mt={4} colorScheme="teal" isLoading={props.isSubmitting} type="submit">
+                  가입하기
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button variant="ghost">Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
