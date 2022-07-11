9d9ad1516e4e68496a41ca07fa3f82b6ea2bcbab
suhwanJang <suhwan9185@gmail.com>
2022-07-11 14:08:45 +0900

    chakra ui 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 import GlobalStyle from '../styles/GlobalStyle';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   );
 }
 
